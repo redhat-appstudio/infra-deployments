660e5b1975b435f9cc9a0286e791276406a52f95
John Duimovich <jduimovich@gmail.com>
2021-11-22 22:27:07 -0500

    support branchs in dev mode

--- hack/development-mode.sh
+++ hack/development-mode.sh
@@ -3,9 +3,10 @@
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/.. 
 
 REPO=$(git config --get remote.origin.url)
- 
-#set the local cluster to point to the current git repo and update the path to development
-$ROOT/hack/util-update-app-of-apps.sh $REPO development
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+#set the local cluster to point to the current git repo and branch and update the path to development
+$ROOT/hack/util-update-app-of-apps.sh $REPO development $BRANCH 
 # reset the default repos in the development directory to be the current git repo
 # this needs to be pushed to your fork to be seen by argocd
-$ROOT/hack/util-set-development-repos.sh $REPO development
+$ROOT/hack/util-set-development-repos.sh $REPO development $BRANCH 
