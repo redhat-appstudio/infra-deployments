43063ab336e05b10b3469a6e63e12af83cf0680e
Flavius Lacatusu <flacatus@redhat.com>
2021-12-14 21:57:38 +0100

    Add script to run e2e tests in Openshift CI
    Signed-off-by: Flavius Lacatusu <flacatus@redhat.com>

--- .ci/oci-e2e-deployment.sh
+++ .ci/oci-e2e-deployment.sh
@@ -12,6 +12,9 @@ export APPLICATION_NAME="gitops"
 #Stop execution on any error
 trap "catchFinish" EXIT SIGINT
 
+command -v yq >/dev/null 2>&1 || { echo "yq is not installed. Aborting."; exit 1; }
+command -v kubectl >/dev/null 2>&1 || { echo "kubectl is not installed. Aborting."; exit 1; }
+
 function catchFinish() {
     JOB_EXIT_CODE=$?
     if [[ "$JOB_EXIT_CODE" != "0" ]]; then
@@ -24,17 +27,49 @@ function catchFinish() {
     exit $JOB_EXIT_CODE
 }
 
-function checkApplicationHealth() {
-    while [ "$(kubectl get application ${APPLICATION_NAME} -n ${APPLICATION_NAMESPACE} -o jsonpath='{.status.health.status}')" != "Healthy" ]; do
-        sleep 3
-        echo "[INFO] Waiting for AppStudio to be ready."
+function provideOpenshiftUser() {
+   htpasswd -c -B -b htpasswd test-user test-user
+   oc create secret generic htpass-secret --from-file=htpasswd=./htpasswd -n openshift-config
+   oc create -f - -o jsonpath='{.metadata.name}' <<EOF
+apiVersion: config.openshift.io/v1
+kind: OAuth
+metadata:
+  name: cluster
+spec:
+  identityProviders:
+  - name: htpasswd
+    mappingMethod: claim 
+    type: HTPasswd
+    htpasswd:
+      fileData:
+        name: htpass-secret
+EOF
+    oc adm policy add-cluster-role-to-user cluster-admin test-user
+
+    echo -e "[INFO] Waiting for htpasswd auth to be working up to 5 minutes"
+    CURRENT_TIME=$(date +%s)
+    ENDTIME=$(($CURRENT_TIME + 300))
+    while [ $(date +%s) -lt $ENDTIME ]; do
+        if oc login -u user -p user --insecure-skip-tls-verify=false; then
+            break
+        fi
+        sleep 10
     done
 }
 
-command -v yq >/dev/null 2>&1 || { echo "yq is not installed. Aborting."; exit 1; }
-command -v kubectl >/dev/null 2>&1 || { echo "kubectl is not installed. Aborting."; exit 1; }
+function downloadE2ERepo() {
+    git clone https://github.com/redhat-appstudio/e2e-tests.git
+}
+
+# Run the script: https://github.com/redhat-appstudio/e2e-tests/blob/main/scripts/run-tests-in-k8s.sh
+function runE2ETestsFramework() {
+   /bin/bash ./e2e-tests/scripts/run-tests-in-k8s.sh
+}
 
 /bin/bash "$WORKSPACE"/hack/bootstrap-cluster.sh
+provideOpenshiftUser
+downloadE2ERepo
 
-export -f checkApplicationHealth
-timeout --foreground 10m bash -c checkApplicationHealth
+# Execute the e2e tests
+export -f runE2ETestsFramework
+timeout --foreground 10m bash -c runE2ETestsFramework
