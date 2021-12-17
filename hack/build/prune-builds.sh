85e06daf0679eebf24c725e3f6e234efccd0bfd3
John Duimovich <jduimovich@gmail.com>
2021-12-16 21:14:09 -0500

    use tekton to prune
    - tekton prunes pipeline runs
    - cleanup uses task to prune build directories
    - TODO use cronjob to prune directories

--- hack/build/prune-builds.sh
+++ hack/build/prune-builds.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-KEEP=$1 
-if [ -z "$KEEP" ] 
-then
-      KEEP=5  
-fi  
-tkn pr delete --keep $KEEP -f
-$SCRIPTDIR/utils/cleanup-pvc.sh  
+
+# Print the Pruning Configuration 
 ISADMIN=$(oc whoami)
-if [ $ISADMIN = "kubeadmin" ]; then 
+if [ "$ISADMIN" = "kubeadmin" ]; then 
+KEEP=$( oc get TektonConfig config -n openshift-pipelines -o 'jsonpath={.spec.pruner.keep}')
+echo
+echo "PipelineRuns will be pruned to last $KEEP"
+echo "Full Config:"
+oc get TektonConfig config -n openshift-pipelines -o 'jsonpath={.spec.pruner}' | jq
+fi 
+
+# Run the pvc internal GC 
+echo
+$SCRIPTDIR/utils/cleanup-pvc.sh  
+echo
+if [ "$ISADMIN" = "kubeadmin" ]; then 
+echo "Pruning Registry"
 oc adm prune images --confirm --registry-url  default-route-openshift-image-registry.apps-crc.testing
 fi 
 
--- hack/build/utils/cleanup-pvc.sh
+++ hack/build/utils/cleanup-pvc.sh
@@ -1,48 +1,13 @@
-#!/bin/bash
-PROJECT=$(oc config view --minify -o 'jsonpath={..namespace}')
+#!/bin/bash 
 
-cat > tmp-pipeline.yaml <<ENV-INLINE-PL-DECL
-apiVersion: tekton.dev/v1beta1
-kind: Pipeline
-metadata:
-  name: cleanup  
-spec: 
-  tasks: 
-    - name: cleanup 
-      taskRef:
-        kind: ClusterTask
-        name: cleanup-build-directories 
-      workspaces:
-        - name: source 
-          workspace: workspace
-  workspaces:
-    - name: workspace
-ENV-INLINE-PL-DECL
+LOG=$(mktemp)
+tkn clustertask start \
+  cleanup-build-directories \
+  --showlog \
+  -w name=source,claimName=app-studio-default-workspace | tee $LOG
 
-cat > tmp-cleanup.yaml <<ENV-INLINE-PR-DECL
-apiVersion: tekton.dev/v1beta1
-kind: PipelineRun
-metadata: 
-  name: cleanup
-spec: 
-  pipelineRef:
-    name: cleanup
-  workspaces:
-    - name: workspace
-      persistentVolumeClaim:
-        claimName: app-studio-default-workspace
-      subPath: "." 
-ENV-INLINE-PR-DECL
-
-oc apply -f tmp-pipeline.yaml   
-BUILD_TAG=$(date +"%Y-%m-%d-%H%M%S")
-PRNAME=cleanup-$BUILD_TAG
-yq -M e ".metadata.name=\"$PRNAME\"" tmp-cleanup.yaml   |  oc apply -f -
-  
-rm -rf tmp-cleanup.yaml tmp-pipeline.yaml 
-tkn pipelinerun logs $PRNAME -f
-tkn pipelinerun delete $PRNAME -f
-tkn pipeline delete cleanup -f
-
+NAME=$(grep "TaskRun started" $LOG  | cut -d ' ' -f 3)
+echo $NAME 
+oc delete taskrun $NAME
 
  
