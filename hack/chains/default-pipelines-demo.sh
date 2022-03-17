6ed2e3f538c6ddbebeb4348d1cb758f0256a106b
Michal Kovarik <mkovarik@redhat.com>
2022-03-17 17:23:07 +0100

    Build definitions update (#184)
    from https://github.com/redhat-appstudio/build-definitions 93dac682ee16a08634d593b836758e1d4fa8d967
    
    Co-authored-by: Build deployer <>

--- hack/chains/default-pipelines-demo.sh
+++ hack/chains/default-pipelines-demo.sh
@@ -22,7 +22,7 @@ title "Push a fresh sha to ensure a rebuild occurs"
 #
 title "Install latest pipeline bundle"
 $ROOT/hack/build/utils/install-pipelines.sh \
-  quay.io/redhat-appstudio/build-templates-bundle:8201a567956ba6d2095d615ea2c0f6ab35f9ba5f
+  quay.io/redhat-appstudio/build-templates-bundle:93dac682ee16a08634d593b836758e1d4fa8d967
 
 title "Run the build pipeline and wait for it to complete"
 $ROOT/hack/build/build.sh $APP_URL
