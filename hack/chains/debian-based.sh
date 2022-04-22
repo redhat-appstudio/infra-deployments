82f685addf3b6b83bccf328bc3dc1c16d146596e
Ken Benoit <kbenoit@redhat.com>
2022-04-22 08:39:19 -0400

    Added checks for pre-existing binary in path so that install is not triggered when pre-req manually installed prior

--- hack/chains/debian-based.sh
+++ hack/chains/debian-based.sh
@@ -2,25 +2,35 @@
 # This script sets up the pre-requisites for running the chains demos on a
 # Debian-based OS (Debian, Ubuntu, etc).
 
+exist_or_install () {
+    local check_command=$1
+    local install_command=$2
+    
+    eval $check_command
+    local status=$?
+
+    if [[ $status -eq 127 ]]
+    then
+        eval $install_command
+    fi
+} # end exist_or_install
+
 # Install the tekton cli
-VERSION=0.23.1
-BINARY=Linux_x86_64
-curl -LO https://github.com/tektoncd/cli/releases/download/v${VERSION}/tkn_${VERSION}_${BINARY}.tar.gz
-sudo tar xvzf tkn_${VERSION}_${BINARY}.tar.gz -C /usr/local/bin/ tkn
+exist_or_install "tkn version" "VERSION=0.23.1; BINARY=Linux_x86_64; curl -LO https://github.com/tektoncd/cli/releases/download/v${VERSION}/tkn_${VERSION}_${BINARY}.tar.gz && sudo tar xvzf tkn_${VERSION}_${BINARY}.tar.gz -C /usr/local/bin/ tkn"
 
 sudo apt-get update -y
 
 # Install go
-sudo apt-get install golang
+exist_or_install "go version" "sudo apt-get install golang"
 
 # Install docker
-sudo apt-get install docker.io
+exist_or_install "docker version" "sudo apt-get install docker.io"
 
 # Install skopeo
-sudo apt-get install skopeo
+exist_or_install "skopeo -v" "sudo apt-get install skopeo"
 
 # Install rekor cli
-go install github.com/sigstore/rekor/cmd/rekor-cli@latest
+exist_or_install "rekor-cli version" "go install github.com/sigstore/rekor/cmd/rekor-cli@latest"
 
 # Install cosign
-go install github.com/sigstore/cosign/cmd/cosign@latest
+exist_or_install "cosign version" "go install github.com/sigstore/cosign/cmd/cosign@latest"
--- hack/chains/macos-based.sh
+++ hack/chains/macos-based.sh
@@ -2,22 +2,35 @@
 # This script sets up the pre-requisites for running the chains demos on a
 # Mac-based OS.
 
+exist_or_install () {
+    local check_command=$1
+    local install_command=$2
+    
+    eval $check_command
+    local status=$?
+
+    if [[ $status -eq 127 ]]
+    then
+        eval $install_command
+    fi
+} # end exist_or_install
+
 brew update
 
 # Install the tekton cli
-brew install tektoncd-cli
+exist_or_install "tkn version" "brew install tektoncd-cli"
 
-# Install go
-brew install go
+# Install go (needed for installing rekor cli)
+exist_or_install "go version" "brew install go"
 
 # Install docker
-brew install docker
+exist_or_install "docker version" "brew install docker"
 
 # Install skopeo
-brew install skopeo
+exist_or_install "skopeo -v" "brew install skopeo"
 
 # Install rekor cli
-go install github.com/sigstore/rekor/cmd/rekor-cli@latest
+exist_or_install "rekor-cli version" "go install github.com/sigstore/rekor/cmd/rekor-cli@latest"
 
 # Install cosign
-brew install cosign
+exist_or_install "cosign version" "brew install cosign"
