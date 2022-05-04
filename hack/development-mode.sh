01fb52ea4c24a63be2ec51ffb9ca076af4db9692
Josef Kopriva <jkopriva@redhat.com>
2022-05-04 16:50:37 +0200

    Add setup for quality dashboard route for current cluster
    Signed-off-by: Josef Kopriva <jkopriva@redhat.com>

--- hack/development-mode.sh
+++ hack/development-mode.sh
@@ -19,6 +19,9 @@ $ROOT/hack/util-set-development-repos.sh $MY_GIT_REPO_URL development $MY_GIT_BR
 # setup is not needed
 $ROOT/hack/util-set-spi-api-server.sh "$SPI_API_SERVER"
 
+# set backend route for quality dashboard for currrent cluster
+$ROOT/hack/util-set-quality-dashboard-backend-route.sh
+
 if [ -n "$MY_GITHUB_ORG" ]; then
     $ROOT/hack/util-set-github-org $MY_GITHUB_ORG
 fi
--- hack/preview.sh
+++ hack/preview.sh
@@ -51,6 +51,9 @@ $ROOT/hack/util-set-development-repos.sh $MY_GIT_REPO_URL development $PREVIEW_B
 # setup is not needed
 $ROOT/hack/util-set-spi-api-server.sh "$SPI_API_SERVER"
 
+# set backend route for quality dashboard for currrent cluster
+$ROOT/hack/util-set-quality-dashboard-backend-route.sh
+
 if [ -n "$MY_GITHUB_ORG" ]; then
     $ROOT/hack/util-set-github-org $MY_GITHUB_ORG
 fi
