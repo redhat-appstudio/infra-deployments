8c0a38618b9c34e4ca9b0e05ad197ac83665ff84
flacatus <flacatus@redhat.com>
2022-06-15 12:36:28 +0200

    fix build service script

--- .ci/oci-e2e-deployment.sh
+++ .ci/oci-e2e-deployment.sh
@@ -15,6 +15,13 @@ export MY_GITHUB_ORG="redhat-appstudio-qe"
 export MY_GITHUB_TOKEN="${GITHUB_TOKEN}"
 export E2E_APPLICATIONS_NAMESPACE=appstudio-e2e-test
 
+# Environment variable used to override the default "protected" image repository in HAS
+# https://github.com/redhat-appstudio/application-service/blob/6b9d21b8f835263b2e92f1e9343a1453caa2e561/gitops/generate_build.go#L50
+# Users are allowed to push images to this repo only in case the image contains a tag that consists of "<USER'S_NAMESPACE_NAME>-<CUSTOM-TAG>"
+# For example: "quay.io/redhat-appstudio-qe/test-images-protected:appstudio-e2e-test-mytag123"
+export HAS_DEFAULT_IMAGE_REPOSITORY="quay.io/redhat-appstudio-qe/test-images-protected"
+
+
 export PATH=$PATH:/tmp/bin
 mkdir -p /tmp/bin
 
