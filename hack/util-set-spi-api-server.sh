5c182174d783137d9fed7426c530c8ed8d9c155e
Michal Vala <mvala@redhat.com>
2022-07-21 19:58:15 +0200

    fix spi patches

--- hack/util-set-spi-api-server.sh
+++ hack/util-set-spi-api-server.sh
@@ -6,23 +6,6 @@ API_SERVER=$1
 
 TMP_FILE=$(mktemp)
 
-cat $PATCH_FILE | jq '
-    map(
-        if (.op == "add" and .path == "/spec/template/spec/containers/0/env") then
-            {
-                "op": .op,
-                "path": .path,
-                "value": [
-                    .value[] | if .name == "API_SERVER" then
-                        {"name": "API_SERVER", "value": "'"$API_SERVER"'"}
-                    else
-                        .
-                    end
-                ]
-            }
-        else
-            .
-        end
-    )' > "$TMP_FILE"
+cat $PATCH_FILE | jq --arg API_SERVER "${API_SERVER}" '.value.value = $API_SERVER' > "$TMP_FILE"
 
 mv "$TMP_FILE" "$PATCH_FILE"
