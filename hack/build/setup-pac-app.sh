d1c03637b632c4b74f86e46b787f28e84ac74a4b
Michal Kovarik <mkovarik@redhat.com>
2022-08-01 10:58:58 +0200

    Appstudio Pipelines-as-code integration (#477)
    Use pipelines-as-code instead of triggers.

---
+++ hack/build/setup-pac-app.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# Inspired by implementation by Will Haley at:
+#   http://willhaley.com/blog/generate-jwt-with-bash/
+
+# Shared content to use as template
+header_template='{
+    "typ": "JWT",
+    "kid": "0001",
+    "iss": "https://stackoverflow.com/questions/46657001/how-do-you-create-an-rs256-jwt-assertion-with-bash-shell-scripting"
+}'
+
+now=$(date +%s)
+build_header() {
+        jq -c \
+                --arg iat_str "$now" \
+                --arg alg "${1:-HS256}" \
+        '
+        ($iat_str | tonumber) as $iat
+        | .alg = $alg
+        | .iat = $iat
+        | .exp = ($iat + 10)
+        ' <<<"$header_template" | tr -d '\n'
+}
+
+b64enc() { openssl enc -base64 -A | tr '+/' '-_' | tr -d '='; }
+json() { jq -c . | LC_CTYPE=C tr -d '\n'; }
+hs_sign() { openssl dgst -binary -sha"${1}" -hmac "$2"; }
+rs_sign() { openssl dgst -binary -sha"${1}" -sign <(printf '%s\n' "$2"); }
+
+sign() {
+        local algo payload header sig secret=$3
+        algo=${1:-RS256}; algo=${algo^^}
+        header=$(build_header "$algo") || return
+        payload=${2:-$test_payload}
+        signed_content="$(json <<<"$header" | b64enc).$(json <<<"$payload" | b64enc)"
+        case $algo in
+                HS*) sig=$(printf %s "$signed_content" | hs_sign "${algo#HS}" "$secret" | b64enc) ;;
+                RS*) sig=$(printf %s "$signed_content" | rs_sign "${algo#RS}" "$secret" | b64enc) ;;
+                *) echo "Unknown algorithm" >&2; return 1 ;;
+        esac
+        printf '%s.%s\n' "${signed_content}" "${sig}"
+}
+payload="{ \"iss\": $GITHUB_APP_ID, \"iat\": ${now}, \"exp\": $((now+10)) }"
+
+if ! oc get -n pipelines-as-code secret pipelines-as-code-secret &>/dev/null; then
+  oc create namespace pipelines-as-code 2>/dev/null
+  webhook_secret=$(openssl rand -hex 20)
+  token=$(sign rs256 "$payload" "$(echo $GITHUB_APP_PRIVATE_KEY | base64 -d)")
+  oc -n pipelines-as-code create secret generic pipelines-as-code-secret \
+        --from-literal github-private-key="$(echo $GITHUB_APP_PRIVATE_KEY | base64 -d)" \
+        --from-literal github-application-id="$GITHUB_APP_ID" \
+        --from-literal webhook.secret="$webhook_secret"
+  webhook_url=$(oc whoami --show-console | sed 's/console-openshift-console/pipelines-as-code-controller-pipelines-as-code/')
+  curl \
+    -X PATCH \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: Bearer $token" \
+    https://api.github.com/app/hook/config \
+    -d "{\"content_type\":\"json\",\"insecure_ssl\":\"1\",\"secret\":\"$webhook_secret\",\"url\":\"$webhook_url\"}"
+fi
