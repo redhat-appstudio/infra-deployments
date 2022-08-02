9fd52aee074d5b03ad3dbf5ac6aff228486ca3b7
Michal Kovarik <mkovarik@redhat.com>
2022-08-02 10:53:48 +0200

    Scripts cleanup and documentation update
    - Removed unused scripts
    - Removed Development mode since preview mode is being developed
      actively
    - Removed old scripts for build which were related to Milestone 2
    - Updated README files
      - Separated gitops README file
      - Updated build README file and removed duplicities from main README
        file
      - Added info about setting tekton bundles
      - Added info about shared secrets

--- hack/build/ls-builds.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash 
-
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-oc get pipelineruns --no-headers -o custom-columns=":metadata.name" | \
-xargs -n 1 -I {}  $SCRIPTDIR/ls-build.sh {}
