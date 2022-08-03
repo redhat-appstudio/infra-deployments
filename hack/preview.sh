7fc42504c26ed4ce7d611cfa64bbf769731bf0d3
Michal Vala <mvala@redhat.com>
2022-08-03 15:55:55 +0200

    eh

--- hack/preview.sh
+++ hack/preview.sh
@@ -39,6 +39,7 @@ $ROOT/hack/util-set-development-repos.sh $MY_GIT_REPO_URL development $PREVIEW_B
 # set the API server which SPI uses to authenticate users to empty string (by default) so that multi-cluster
 # setup is not needed
 yq -i e ".0.value.0.value=\"$SPI_API_SERVER\"" $ROOT/components/spi/oauth-service-deployment-patch.json
+$ROOT/hack/util-set-spi-vault-host.sh
 
 # set backend route for quality dashboard for current cluster
 $ROOT/hack/util-set-quality-dashboard-backend-route.sh
--- hack/util-set-spi-vault-host.sh
+++ hack/util-set-spi-vault-host.sh
@@ -5,7 +5,7 @@ PATCH_FILE="$ROOT/components/spi/config-patch.json"
 
 if [ -z ${1} ]; then
     CLUSTER_URL_HOST=$(oc whoami --show-console|sed 's|https://console-openshift-console.apps.||')
-    VAULT_HOST="https://vault-spi-vault.apps.${CLUSTER_URL_HOST}"
+    VAULT_HOST="https://spi-vault-spi-system.apps.${CLUSTER_URL_HOST}"
 else
     VAULT_HOST=${1}
 fi
