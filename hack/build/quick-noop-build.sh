118eb2982b66f5073403d4d1d2d88b9d926b71ef
Michal Kovarik <mkovarik@redhat.com>
2022-08-04 09:48:48 +0200

    Scripts cleanup and documentation update (#557)
    - Removed unused scripts
    - Removed Development mode since preview mode is being developed
      actively
    - Removed old scripts for build which were related to Milestone 2
    - Updated README files
      - Separated gitops README file
      - Updated build README file and removed duplicities from main README
        file
      - Added info about setting tekton bundles
      - Added info about shared secrets

--- hack/build/quick-noop-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
-# this does a quick noop build
-$SCRIPTDIR/build.sh https://github.com/jduimovich/single-container-app noop
- 
--- hack/build/test-known-build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
-$SCRIPTDIR/m2-builds.sh 
- 
