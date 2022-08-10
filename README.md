c7bb655c2b86917542958a2f52957d85ce8f142e
Bama Charan Kundu <bamachrn@gmail.com>
2022-08-10 20:33:30 +0530

    Updated Readme datails for running the setup_observability script
    Signed-off-by: Bama Charan Kundu <bamachrn@gmail.com>

--- README.md
+++ README.md
@@ -208,12 +208,16 @@ More information about quality-dashboard you can found in repo: `https://github.
 
 ## Setting up observability stack
 
-Script to set up observabilty stack (i.e. prometheus and grafana)
+Script to set up observability stack (i.e. prometheus and grafana)
 - [hack/setup_observability.sh](hack/setup_observability.sh)
 This script requires few things
-* [Githud oauth](https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps) tokens for authentication of the components
+* [Github oauth](https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps) tokens for authentication of the components
 * [Github Cookie secret](https://oauth2-proxy.github.io/oauth2-proxy/docs/configuration/overview)
 * [oc](https://docs.openshift.com/container-platform/4.11/cli_reference/openshift_cli/getting-started-cli.html) binary installed and configured to have admin access to the cluster
+
+for running the `hack/setup_observability.sh` script
+1. Update the values for the variables from github oauth
+2. ```$ ./hack/setup_observability.sh```
 
 ## FAQ
 
--- hack/setup_observability.sh
+++ hack/setup_observability.sh
@@ -11,14 +11,24 @@ cd $ROOT/hack/monitoring
 #default is openshift-customer-monitoring
 export MONITORING_NAMESPACE="openshift-customer-monitoring"
 
+#Values for the below variables need to be replaced with actual oauth values from github
+#Check readme for details about how to get github oauth details
 #Github access tokens for prometheus oauth
 export PROMETHEUS_GITHUB_CLIENT_ID="<base64_encoded_github_client_id_for_prometheus>"
 export PROMETHEUS_GITHUB_CLIENT_SECRET="<base64_encoded_github_client_secret_for_prometheus>"
 export PROMETHEUS_GITHUB_COOKIE_SECRET="<base64_encoded_github_cookie_secret>"
 export PROMETHEUS_K8S_SECRET_NAME="<prometheus_secret_for_serviceaccounts>"
 
+#Github access tokens for grafana oauth
+export GRAFANA_GITHUB_CLIENT_ID="<base64_encoded_github_clientid_for_grafana>"
+export GRAFNA_GITHUB_CLIENT_SECRET="<base64_encoded_github_client_secret_for_grafana>"
+export GRAFANA_GITHUB_COOKIE_SECRET="<base64_encoded_github_cookie_secret>"
+export GRAFANA_ADMIN_USER="<admin_userid_for_grafana>"
+export GRAFANA_ADMIN_PASSWD="<password_for_grafana_admin_user>"
+
 #Install prometheus operator
 oc process -p MONITORING_NAMESPACE=$MONITORING_NAMESPACE -f install_prometheus.yaml|oc apply -f -
+
 #Configure prometheus with github oauth
 oc process -p MONITORING_NAMESPACE=$MONITORING_NAMESPACE -p PROMETHEUS_GITHUB_CLIENT_ID=$PROMETHEUS_GITHUB_CLIENT_ID -p PROMETHEUS_GITHUB_CLIENT_SECRET=$PROMETHEUS_GITHUB_CLIENT_SECRET -p PROMETHEUS_GITHUB_COOKIE_SECRET=$PROMETHEUS_GITHUB_COOKIE_SECRET -p PROMETHEUS_K8S_SECRET_NAME=$PROMETHEUS_K8S_SECRET_NAME -f configure_prometheus.yaml|oc apply -f -
 
@@ -29,13 +39,6 @@ oc policy add-role-to-user view system:serviceaccount:openshift-customer-monitor
 
 #Install grafana operator
 oc process -p MONITORING_NAMESPACE=$MONITORING_NAMESPACE -f install_grafana.yaml|oc apply -f -
-
-#Github access tokens for grafana oauth
-export GRAFANA_GITHUB_CLIENT_ID="<base64_encoded_github_clientid_for_grafana>"
-export GRAFNA_GITHUB_CLIENT_SECRET="<base64 encoded github client secret for grafana>"
-export GRAFANA_GITHUB_COOKIE_SECRET="<base64 encoded github cookie secret>"
-export GRAFANA_ADMIN_USER="<admin userid for grafana>"
-export GRAFANA_ADMIN_PASSWD="<password for grafana admin user>"
 
 #Configure grafana with github oauth
 oc process -p MONITORING_NAMESPACE=$MONITORING_NAMESPACE -p GRAFANA_GITHUB_CLIENT_ID=$GRAFANA_GITHUB_CLIENT_ID -p GRAFNA_GITHUB_CLIENT_SECRET=$GRAFNA_GITHUB_CLIENT_SECRET -p GRAFANA_GITHUB_COOKIE_SECRET=$GRAFANA_GITHUB_COOKIE_SECRET -p GRAFANA_ADMIN_PASSWD=$GRAFANA_ADMIN_PASSWD -p GRAFANA_ADMIN_USER=$GRAFANA_ADMIN_USER -f configure_grafana.yaml|oc apply -f -
