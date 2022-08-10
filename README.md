7bf0b264f5074a4c4a6cef95ecfd4a79bba90f6a
Bama Charan Kundu <bamachrn@gmail.com>
2022-08-10 19:31:28 +0530

    Moved monitoring to hack dir and added README
    Signed-off-by: Bama Charan Kundu <bamachrn@gmail.com>

--- README.md
+++ README.md
@@ -206,6 +206,15 @@ By default the frontend is using AppStudio Staging cluster for backend. If you w
 
 More information about quality-dashboard you can found in repo: `https://github.com/redhat-appstudio/quality-dashboard`.
 
+## Setting up observability stack
+
+Script to set up observabilty stack (i.e. prometheus and grafana)
+- [hack/setup_observability.sh](hack/setup_observability.sh)
+This script requires few things
+* [Githud oauth](https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps) tokens for authentication of the components
+* [Github Cookie secret](https://oauth2-proxy.github.io/oauth2-proxy/docs/configuration/overview)
+* [oc](https://docs.openshift.com/container-platform/4.11/cli_reference/openshift_cli/getting-started-cli.html) binary installed and configured to have admin access to the cluster
+
 ## FAQ
 
 Other questions? Ask on `#wg-developer-appstudio`.
---
+++ hack/setup_observability.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#This script installs the prometheus and granafa operator
+#from community operator hub. It then configures them with
+#github oauth
+
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
+
+cd $ROOT/hack/monitoring
+
+#Namespace to which we want to install observability stack
+#default is openshift-customer-monitoring
+export MONITORING_NAMESPACE="openshift-customer-monitoring"
+
+#Github access tokens for prometheus oauth
+export PROMETHEUS_GITHUB_CLIENT_ID="<base64_encoded_github_client_id_for_prometheus>"
+export PROMETHEUS_GITHUB_CLIENT_SECRET="<base64_encoded_github_client_secret_for_prometheus>"
+export PROMETHEUS_GITHUB_COOKIE_SECRET="<base64_encoded_github_cookie_secret>"
+export PROMETHEUS_K8S_SECRET_NAME="<prometheus_secret_for_serviceaccounts>"
+
+#Install prometheus operator
+oc process -p MONITORING_NAMESPACE=$MONITORING_NAMESPACE -f install_prometheus.yaml|oc apply -f -
+#Configure prometheus with github oauth
+oc process -p MONITORING_NAMESPACE=$MONITORING_NAMESPACE -p PROMETHEUS_GITHUB_CLIENT_ID=$PROMETHEUS_GITHUB_CLIENT_ID -p PROMETHEUS_GITHUB_CLIENT_SECRET=$PROMETHEUS_GITHUB_CLIENT_SECRET -p PROMETHEUS_GITHUB_COOKIE_SECRET=$PROMETHEUS_GITHUB_COOKIE_SECRET -p PROMETHEUS_K8S_SECRET_NAME=$PROMETHEUS_K8S_SECRET_NAME -f configure_prometheus.yaml|oc apply -f -
+
+#Give prometheus service account access to scrape the operators
+oc policy add-role-to-user view system:serviceaccount:openshift-customer-monitoring:prometheus-k8s -n toolchain-host-operator
+oc policy add-role-to-user view system:serviceaccount:openshift-customer-monitoring:prometheus-k8s -n toolchain-member-operator
+
+
+#Install grafana operator
+oc process -p MONITORING_NAMESPACE=$MONITORING_NAMESPACE -f install_grafana.yaml|oc apply -f -
+
+#Github access tokens for grafana oauth
+export GRAFANA_GITHUB_CLIENT_ID="<base64_encoded_github_clientid_for_grafana>"
+export GRAFNA_GITHUB_CLIENT_SECRET="<base64 encoded github client secret for grafana>"
+export GRAFANA_GITHUB_COOKIE_SECRET="<base64 encoded github cookie secret>"
+export GRAFANA_ADMIN_USER="<admin userid for grafana>"
+export GRAFANA_ADMIN_PASSWD="<password for grafana admin user>"
+
+#Configure grafana with github oauth
+oc process -p MONITORING_NAMESPACE=$MONITORING_NAMESPACE -p GRAFANA_GITHUB_CLIENT_ID=$GRAFANA_GITHUB_CLIENT_ID -p GRAFNA_GITHUB_CLIENT_SECRET=$GRAFNA_GITHUB_CLIENT_SECRET -p GRAFANA_GITHUB_COOKIE_SECRET=$GRAFANA_GITHUB_COOKIE_SECRET -p GRAFANA_ADMIN_PASSWD=$GRAFANA_ADMIN_PASSWD -p GRAFANA_ADMIN_USER=$GRAFANA_ADMIN_USER -f configure_grafana.yaml|oc apply -f -
+
+#Configure grafana datasource
+oc apply -f grafanadatasource_prometheus.yaml
+
+#Configure grafana dashboard
+oc apply -f grafanadashboard_sandbox_usage.yaml
+
+cd $ROOT
