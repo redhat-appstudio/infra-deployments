3711bd56bf9205e05dbeb726c02cf378df3f2ec7
Meer Sawood <msawood@redhat.com>
2022-08-16 17:59:45 +0530

    chore: Add authentication to the Quality Dashboard (#601)

--- hack/util-set-quality-dashboard-backend-route.sh
+++ hack/util-set-quality-dashboard-backend-route.sh
@@ -4,4 +4,4 @@ ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
 KUSTOMIZATION_FILE="$ROOT/components/quality-dashboard/frontend/kustomization.yaml"
 CLUSTER_URL_HOST=$(oc whoami --show-console|sed 's|https://console-openshift-console.apps.||')
 
-sed -i "s|appstudio-stage.x99m.p1.openshiftapps.com|${CLUSTER_URL_HOST}|" "$KUSTOMIZATION_FILE"
+yq e -i "(.configMapGenerator[].literals[] | select(. == \"*BACKEND_ROUTE*\")) = \"BACKEND_ROUTE=https://quality-backend-route-quality-dashboard.apps.$CLUSTER_URL_HOST\"" "$KUSTOMIZATION_FILE"
