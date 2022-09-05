48ad5cc44fcda4751f205efb2df9d76104d19616
Matous Jobanek <mjobanek@redhat.com>
2022-09-05 12:53:49 +0200

    update main branch with kcp manifests, scripts, and structure (#707)

--- hack/util-set-quality-dashboard-backend-route.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
-KUSTOMIZATION_FILE="$ROOT/components/quality-dashboard/frontend/kustomization.yaml"
-CLUSTER_URL_HOST=$(oc whoami --show-console|sed 's|https://console-openshift-console.apps.||')
-
-yq e -i "(.configMapGenerator[].literals[] | select(. == \"*BACKEND_ROUTE*\")) = \"BACKEND_ROUTE=https://quality-backend-route-quality-dashboard.apps.$CLUSTER_URL_HOST\"" "$KUSTOMIZATION_FILE"
