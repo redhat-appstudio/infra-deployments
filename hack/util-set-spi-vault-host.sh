5dad396c956ddc75e2ed5424fb3d35192d95b58e
Luiz Carvalho <lucarval@redhat.com>
2022-09-09 12:03:25 -0400

    Fix spi domain resolution
    Not all cluster have `apps.` as part of the domain.
    
    Signed-off-by: Luiz Carvalho <lucarval@redhat.com>

--- hack/util-set-spi-vault-host.sh
+++ hack/util-set-spi-vault-host.sh
@@ -4,8 +4,8 @@ ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
 PATCH_FILE="$ROOT/components/spi/config-patch.json"
 
 if [ -z ${1} ]; then
-    CLUSTER_URL_HOST=$(oc whoami --show-console|sed 's|https://console-openshift-console.apps.||')
-    VAULT_HOST="https://spi-vault-spi-system.apps.${CLUSTER_URL_HOST}"
+    CLUSTER_URL_HOST=$(kubectl get ingresses.config.openshift.io cluster --template={{.spec.domain}})
+    VAULT_HOST="https://spi-vault-spi-system.${CLUSTER_URL_HOST}"
 else
     VAULT_HOST=${1}
 fi
@@ -15,5 +15,5 @@ TMP_FILE=$(mktemp)
 cat $PATCH_FILE | jq --arg VAULT_HOST "${VAULT_HOST}" '.[0].value = $VAULT_HOST' > "$TMP_FILE"
 mv "$TMP_FILE" "$PATCH_FILE"
 
-# because we can't be sure that target testing cluster has valid signed cert, we allow insecure tls connection to Vault 
+# because we can't be sure that target testing cluster has valid signed cert, we allow insecure tls connection to Vault
 yq e -i '.patches += {"target": {"kind": "Deployment","name": "controller-manager|oauth-service"}, "path": "insecuretls-patch.json"}' $ROOT/components/spi/kustomization.yaml
