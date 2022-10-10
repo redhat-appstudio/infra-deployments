968087fb62391ff74d75c357a6445a4498d09d35
Michal Kovarik <mkovarik@redhat.com>
2022-10-10 14:36:36 +0200

    Set TEKTON_RESULTS creds for pipeline-service
    Script for installation of pipeline-service now requires
    TEKTON_RESULTS_DATABASE_USER and TEKTON_RESULTS_DATABASE_PASSWORD env

--- hack/install-pipeline-service.sh
+++ hack/install-pipeline-service.sh
@@ -10,6 +10,7 @@ PIPELINE_SERVICE_DIR=$(mktemp -d)
 git clone --depth 1 https://github.com/openshift-pipelines/pipeline-service/ $PIPELINE_SERVICE_DIR
 export WORK_DIR="${PIPELINE_SERVICE_DIR}/gitops/sre/"
 export WORKSPACE_DIR=$WORK_DIR
+export TEKTON_RESULTS_DATABASE_USER=${TEKTON_RESULTS_DATABASE_USER:-"tekton-results"} TEKTON_RESULTS_DATABASE_PASSWORD=${TEKTON_RESULTS_DATABASE_PASSWORD:-"tekton-results"}
 
 KUBECONFIG=$KCP_KUBECONFIG $PIPELINE_SERVICE_DIR/images/access-setup/content/bin/setup_kcp.sh --kcp-workspace $PIPELINE_SERVICE_WORKSPACE --kcp-org $ROOT_WORKSPACE
 
