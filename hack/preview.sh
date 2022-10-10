ed24d857fbc41dbb821375677f50354ed3f92b64
Sergii Kabashniuk <skabashnyuk@users.noreply.github.com>
2022-10-10 17:29:15 +0300

    Update to the latest spi v0.8.x (#816)

--- hack/preview.sh
+++ hack/preview.sh
@@ -151,7 +151,7 @@ if echo $APPS | grep -q spi-vault; then
     echo "Initializing Vault"
     export VAULT_KUBE_CONFIG=${CLUSTER_KUBECONFIG}
     export VAULT_NAMESPACE=spi-vault
-    bash <(curl -s https://raw.githubusercontent.com/redhat-appstudio/service-provider-integration-operator/13aff67f9ad6f5b583e5e8d952441bdd02739fa7/hack/vault-init.sh)
+    bash <(curl -s https://raw.githubusercontent.com/redhat-appstudio/service-provider-integration-operator/c35ac7b1f5c27e6f4b023ff06b4db854246762ed/hack/vault-init.sh)
     SPI_APP_ROLE_FILE=.tmp/approle_secret.yaml
     if [ -f "$SPI_APP_ROLE_FILE" ]; then
         echo "$SPI_APP_ROLE_FILE exists."
