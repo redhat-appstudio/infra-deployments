7a728179bf3d1bfa952db640eba56c2599ce6c49
Flavius Lacatusu <flacatus@redhat.com>
2023-01-18 10:39:53 +0100

    Update permissions

--- hack/bootstrap-cluster.sh
+++ hack/bootstrap-cluster.sh
@@ -161,7 +161,7 @@ echo "Setting secrets for Dora metrics exporter"
 kubectl create namespace dora-metrics -o yaml --dry-run=client | oc apply -f-
 if ! kubectl get secret -n dora-metrics exporters-secret &>/dev/null; then
   kubectl create secret generic exporters-secret -n dora-metrics \
-    --from-literal=github=${GITHUB_TOKEN:-""} \
+    --from-literal=github=${MY_GITHUB_TOKEN:-""}
 fi
 
 ARGO_CD_ROUTE=$(kubectl get \
