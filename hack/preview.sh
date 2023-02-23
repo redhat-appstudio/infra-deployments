44cb307824fa97d1ec9a830026e423f2050a275a
Michal Kovarik <mkovarik@redhat.com>
2023-02-23 10:57:35 +0100

    Update version and allow secret-store access namespace

--- hack/preview.sh
+++ hack/preview.sh
@@ -189,7 +189,6 @@ sed -i.bak "s/rekor-server.enterprise-contract-service.svc/$rekor_server/" $ROOT
 [ -n "${IMAGE_CONTROLLER_IMAGE_TAG}" ] && yq -i e "(.images.[] | select(.name==\"quay.io/shbose/image-controller\")) |=.newTag=\"${IMAGE_CONTROLLER_IMAGE_TAG}\"" $ROOT/components/image-controller/base/kustomization.yaml
 
 [[ -n "${PIPELINE_PR_OWNER}" && "${PIPELINE_PR_SHA}" ]] && yq -i e "(.resources[] | select(. ==\"*github.com/openshift-pipelines/pipeline-service*\")) |= \"git::https://github.com/${PIPELINE_PR_OWNER}/pipeline-service.git/operator/gitops/argocd/pipeline-service?ref=${PIPELINE_PR_SHA}\"" $ROOT/components/pipeline-service/development/kustomization.yaml
->>>>>>> origin/main
 
 if ! git diff --exit-code --quiet; then
     git commit -a -m "Preview mode, do not merge into main"
