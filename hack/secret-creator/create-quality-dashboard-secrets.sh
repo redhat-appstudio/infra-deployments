6496df26bb0669fe7dcfa74cc37adf852eb5f721
Meer Sawood <msawood@redhat.com>
2023-03-17 12:43:19 +0530

    fix: qualtiy dashboard auth secret on stage

--- hack/secret-creator/create-quality-dashboard-secrets.sh
+++ hack/secret-creator/create-quality-dashboard-secrets.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+TEMP_USERS_VALUE='test:$2y$05$YXriyqEu7vzNMJklogXm3eaQ6/Plhip7zHXVBDwfJdlsBRpAI8oTu'
+
 main() {
     echo "Setting secrets for Quality Dashboard"
     kubectl create namespace quality-dashboard -o yaml --dry-run=client | oc apply -f-
@@ -12,6 +14,11 @@ main() {
             --from-literal=storage-database=quality \
             --from-literal=github-token=REPLACE_GITHUB_TOKEN \
             --from-literal=jira-token=REPLACE_JIRA_TOKEN
+    fi
+    if ! kubectl get secret -n quality-dashboard quality-dashboard-auth &>/dev/null; then
+        kubectl create secret generic quality-dashboard-auth \
+            --namespace=quality-dashboard \
+            --from-literal=users.htpasswd=$TEMP_USERS_VALUE 
     fi
 }
 
