6d0c97bfac5556421c26e09933118d523bb2a43a
Flavius Lacatusu <flacatus@redhat.com>
2023-05-12 00:47:35 +0200

    Declare github token list to use more than 1 token in has controller (#1809)

--- hack/secret-creator/create-github-secret.sh
+++ hack/secret-creator/create-github-secret.sh
@@ -2,10 +2,17 @@
 
 main() {
     local github_token=${1:?"Github token was not provided"}
+    local tokens_list=${2}
 
-    echo "Creating a secret with a token for Github"
     oc create namespace application-service --dry-run=client -o yaml | oc apply -f -
-    oc create -n application-service secret generic has-github-token --from-literal=token="$github_token" --dry-run=client -o yaml | oc apply -f -
+    # if not multiple tokens provided then use legacy one.
+    if [[ -z "${tokens_list}" ]]; then
+        echo "Creating a has secret from legacy token"
+        oc create -n application-service secret generic has-github-token --from-literal=token="$github_token" --dry-run=client -o yaml | oc apply -f -
+    else
+        echo "Creating a secret with multiple tokens from Github"
+        oc create -n application-service secret generic has-github-token --from-literal=tokens="$tokens_list" --dry-run=client -o yaml | oc apply -f -
+    fi
 }
 
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
