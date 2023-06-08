a847adbf2afc305b9ab23fb620f37e34fd5127dc
Sergii Kabashniuk <skabashniuk@redhat.com>
2023-06-08 16:33:43 +0300

    Change config

--- hack/util-patch-spi-config.sh
+++ hack/util-patch-spi-config.sh
@@ -3,16 +3,27 @@
 # Expects up to 3 parameters.
 # 1. is the vault host (defaults to https://vault-spi-vault.apps.<cluster URL>)
 # 2. is the base URL of SPI (defaults to https://spi-oauth-spi-system.apps.<cluster URL>)
+JQ_SCRIPT=$(cat << "EOF"
+map(
+    if (.op == "replace" and .path == "/data/VAULTHOST") then
+        {"op": .op, "path": .path, "value": $VAULTHOST }
+    elif (.op == "replace" and .path == "/data/BASEURL") then
+        {"op": .op, "path": .path, "value": $BASEURL }
+    else
+        .
+    end
+)
+EOF
+)
 
 patchConfig() {
-    if [[ $# -ne 2 ]]; then
+    if [[ $# -ne 1 ]]; then
       echo "invalid number of arguments"$#
       echo "usage:"
-      echo "  $0 PATCH_SCRIPT PATCH_FILE"
+      echo "  $0 PATCH_FILE"
       exit 1
     fi
-  PATCH_SCRIPT=$1
-  PATCH_FILE=$2
+  PATCH_FILE=$1
   echo 'Patching VAULTHOST and BASEURL for '"$PATCH_FILE"
   if [ -z ${1} ]; then
       APPS_BASE_URL=$(oc get ingress.config cluster -o jsonpath='{.spec.domain}')
@@ -33,7 +44,7 @@ patchConfig() {
 
   TMP_FILE=$(mktemp)
 
-  cat "$PATCH_FILE" | jq --arg VAULTHOST "${VAULT_HOST}" --arg BASEURL "${SPI_BASE_URL}" "${PATCH_SCRIPT}" > "$TMP_FILE"
+  cat "$PATCH_FILE" | jq --arg VAULTHOST "${VAULT_HOST}" --arg BASEURL "${SPI_BASE_URL}" "${JQ_SCRIPT}" > "$TMP_FILE"
   cp "$TMP_FILE" "$PATCH_FILE"
 
   rm "$TMP_FILE"
@@ -42,20 +53,9 @@ patchConfig() {
 }
 
 
-JQ_SCRIPT=$(cat << "EOF"
-map(
-    if (.op == "replace" and .path == "/data/VAULTHOST") then
-        {"op": .op, "path": .path, "value": $VAULTHOST }
-    elif (.op == "replace" and .path == "/data/BASEURL") then
-        {"op": .op, "path": .path, "value": $BASEURL }
-    else
-        .
-    end
-)
-EOF
-)
+
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
 
-patchConfig $JQ_SCRIPT "$ROOT/components/spi/overlays/development/config-patch.json"
-patchConfig $JQ_SCRIPT "$ROOT/components/remote-secret-controller/overlays/development/config-patch.json"
+patchConfig "$ROOT/components/spi/overlays/development/config-patch.json"
+patchConfig "$ROOT/components/remote-secret-controller/overlays/development/config-patch.json"
