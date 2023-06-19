8793f8912881807cbe5c960c93ac45f73d18d632
Sergii Kabashniuk <skabashnyuk@users.noreply.github.com>
2023-06-19 12:38:57 +0300

    Remote secret operator deployment (#1917)
    ---------
    
    Signed-off-by: Sergii Kabashniuk <skabashniuk@redhat.com>

--- hack/util-patch-spi-config.sh
+++ hack/util-patch-spi-config.sh
@@ -3,8 +3,6 @@
 # Expects up to 3 parameters.
 # 1. is the vault host (defaults to https://vault-spi-vault.apps.<cluster URL>)
 # 2. is the base URL of SPI (defaults to https://spi-oauth-spi-system.apps.<cluster URL>)
-# 3. is either true or false and defaults to true. When true, Vault is configured to accepts TLS connections with untrusted certificates.
-
 JQ_SCRIPT=$(cat << "EOF"
 map(
     if (.op == "replace" and .path == "/data/VAULTHOST") then
@@ -18,39 +16,36 @@ map(
 EOF
 )
 
+patchConfig() {
+  PATCH_FILE=$1
+  echo 'Patching VAULTHOST and BASEURL for '"$PATCH_FILE"
+  if [ -z ${2} ]; then
+      APPS_BASE_URL=$(oc get ingress.config cluster -o jsonpath='{.spec.domain}')
+      VAULT_HOST="https://vault-spi-vault.${APPS_BASE_URL}"
+  else
+      VAULT_HOST=${2}
+  fi
+
+  if [ -z ${3} ]; then
+      if [ -z $APPS_BASE_URL ]; then
+         APPS_BASE_URL=$(oc get ingress.config cluster -o jsonpath='{.spec.domain}')
+      fi
+      SPI_BASE_URL="https://spi-oauth-spi-system.${APPS_BASE_URL}"
+  else
+      SPI_BASE_URL=${3}
+  fi
+
+
+  TMP_FILE=$(mktemp)
+
+  cat "$PATCH_FILE" | jq --arg VAULTHOST "${VAULT_HOST}" --arg BASEURL "${SPI_BASE_URL}" "${JQ_SCRIPT}" > "$TMP_FILE"
+  cp "$TMP_FILE" "$PATCH_FILE"
+
+  rm "$TMP_FILE"
+
+}
+
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
-PATCH_FILE="$ROOT/components/spi/overlays/development/config-patch.json"
 
-if [ -z ${1} ]; then
-    APPS_BASE_URL=$(oc get ingress.config cluster -o jsonpath='{.spec.domain}')
-    VAULT_HOST="https://vault-spi-vault.${APPS_BASE_URL}"
-else
-    VAULT_HOST=${1}
-fi
-
-if [ -z ${2} ]; then
-    if [ -z $APPS_BASE_URL ]; then
-       APPS_BASE_URL=$(oc get ingress.config cluster -o jsonpath='{.spec.domain}')
-    fi
-    SPI_BASE_URL="https://spi-oauth-spi-system.${APPS_BASE_URL}"
-else
-    SPI_BASE_URL=${2}
-fi
-
-if [ -z ${3} ]; then
-    VAULT_INSECURE_TLS="true"
-else
-    VAULT_INSECURE_TLS=${3}
-fi
-
-TMP_FILE=$(mktemp)
-
-cat $PATCH_FILE | jq --arg VAULTHOST "${VAULT_HOST}" --arg BASEURL "${SPI_BASE_URL}" "${JQ_SCRIPT}" > "$TMP_FILE"
-cp "$TMP_FILE" "$PATCH_FILE"
-
-if [ "$VAULT_INSECURE_TLS" == "true" ]; then
-    cat "$PATCH_FILE" | jq '. += [{"op": "add", "path": "/data/VAULTINSECURETLS", "value": "true"}]' > "$TMP_FILE"
-    cp "$TMP_FILE" "$PATCH_FILE"
-fi
-
-rm "$TMP_FILE"
+patchConfig "$ROOT/components/spi/overlays/development/config-patch.json" "${1}" "${2}"
+patchConfig "$ROOT/components/remote-secret-controller/overlays/development/config-patch.json" "${1}" "${2}"
