fda68234a5e0aacac10b431130852ca5ac6de4cd
Alessandro <abarbaro@redhat.com>
2023-06-29 15:43:19 +0200

    update dora metrics (#2040)

--- hack/secret-creator/create-dora-metrics-exporter-secrets.sh
+++ hack/secret-creator/create-dora-metrics-exporter-secrets.sh
@@ -5,7 +5,7 @@ main() {
     kubectl create namespace dora-metrics -o yaml --dry-run=client | oc apply -f-
     if ! kubectl get secret -n dora-metrics exporters-secret &>/dev/null; then
         kubectl create secret generic exporters-secret -n dora-metrics \
-            --from-literal=github=${github_token:-""}
+            --from-literal=github=${github_token:-""} --from-literal=pager-duty-token=${pager-duty-token:-""}
     fi
 }
 
