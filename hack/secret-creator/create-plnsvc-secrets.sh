0e3bab77f18621b1279d62ad6e9f3eb19c3ae3ca
Avinal Kumar <avinal@redhat.com>
2024-04-01 19:08:15 +0530

    enable ssl/tls for tekton results db connections
    - Adds required YAML config for enabling db ssl connections
    - Uses public certificates for RDS available here: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/UsingWithRDS.SSL.html#UsingWithRDS.SSL.RegionCertificates
    - Enables dbssl for staging environment
    - Adds cert generation for development setup
    - Includes openshift-pipelines/pipeline-service#984
    
    Signed-off-by: Avinal Kumar <avinal@redhat.com>
    
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED

--- hack/secret-creator/create-plnsvc-secrets.sh
+++ hack/secret-creator/create-plnsvc-secrets.sh
@@ -5,6 +5,7 @@ main() {
     create_namespace
     create_db_secret
     create_s3_secret
+    create_db_cert_secret_and_configmap
 }
 
 create_namespace() {
@@ -62,6 +63,49 @@ stringData:
     export MINIO_STORAGE_CLASS_STANDARD="EC:2"
     export MINIO_BROWSER="on"
 EOF
+}
+
+create_db_cert_secret_and_configmap() {
+  echo "Creating DB cert secret" >&2
+  if kubectl get secret -n tekton_results postgresql-tls &>/dev/null; then
+    echo "DB cert secret already exists, skipping creation"
+    return
+  fi
+  mkdir -p .tmp/tekton-results
+  openssl req -newkey rsa:4096 -nodes -text \
+    -keyout ".tmp/tekton-results/root.key" \
+    -out ".tmp/tekton-results/root.csr" \
+    -subj "/CN=postgres-postgresql.tekton-results.svc.cluster.local" \
+    -addext "subjectAltName=DNS:postgres-postgresql.tekton-results.svc.cluster.local" \
+    > /dev/null 2>&1
+  chmod og-rwx ".tmp/tekton-results/root.key"
+  openssl x509 -req -days 7 -text \
+    -signkey ".tmp/tekton-results/root.key" \
+    -in ".tmp/tekton-results/root.csr" \
+    -extfile "/etc/ssl/openssl.cnf" \
+    -extensions v3_ca \
+    -out ".tmp/tekton-results/ca.crt" \
+    > /dev/null 2>&1
+  openssl req -new -nodes -text \
+    -out ".tmp/tekton-results/root.csr" \
+    -keyout ".tmp/tekton-results/tls.key" \
+    -subj "/CN=postgres-postgresql.tekton-results.svc.cluster.local" \
+    -addext "subjectAltName=DNS:postgres-postgresql.tekton-results.svc.cluster.local" \
+    > /dev/null 2>&1
+  chmod og-rwx ".tmp/tekton-results/tls.key"
+  openssl x509 -req -text -days 7 -CAcreateserial \
+    -in ".tmp/tekton-results/root.csr" \
+    -CA ".tmp/tekton-results/ca.crt" \
+    -CAkey ".tmp/tekton-results/root.key" \
+    -out ".tmp/tekton-results/tls.crt" \
+    > /dev/null 2>&1
+  cat ".tmp/tekton-results/ca.crt" ".tmp/tekton-results/tls.crt" > ".tmp/tekton-results/tekton-results-db-ca.pem"
+  kubectl create secret generic -n tekton-results postgresql-tls \
+    --from-file=.tmp/tekton-results/ca.crt \
+    --from-file=.tmp/tekton-results/tls.crt \
+    --from-file=.tmp/tekton-results/tls.key
+  kubectl create configmap -n tekton-results rds-root-crt \
+    --from-file=.tmp/tekton-results/tekton-results-db-ca.pem
 }
 
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
