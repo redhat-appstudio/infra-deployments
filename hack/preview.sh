da0b0bdbf7553dac12ad0fcc0808b05bad4d4468
Max Shaposhnyk <mshaposh@redhat.com>
2024-07-05 17:14:30 +0300

    Remove OAuth service deployment from SPI for development and STG (#4002)
    * Remove OAuth service deployment from SPI
    
    Signed-off-by: Max Shaposhnyk <mshaposh@redhat.com>
    
    * Remove OAuth service deployment from SPI
    
    Signed-off-by: Max Shaposhnyk <mshaposh@redhat.com>
    
    * Remove OAuth service deployment from SPI
    
    Signed-off-by: Max Shaposhnyk <mshaposh@redhat.com>
    
    ---------
    
    Signed-off-by: Max Shaposhnyk <mshaposh@redhat.com>

--- hack/preview.sh
+++ hack/preview.sh
@@ -156,9 +156,6 @@ if [ -n "$DEPLOY_ONLY" ]; then
   done
 fi
 
-# set the API server which SPI uses to authenticate users to empty string (by default) so that multi-cluster
-# setup is not needed
-yq -i e ".0.value=\"$SPI_API_SERVER\"" $ROOT/components/spi/overlays/development/oauth-service-config-patch.json
 # patch the SPI configuration with the Vault host configuration to provided VAULT_HOST variable or to current cluster
 # and the base URL set to the SPI_BASE_URL variable or the URL of the  route to the SPI OAuth service in the current cluster
 # This script also sets up the Vault client to accept insecure TLS connections so that the custom vault host doesn't have
