9b297539d2a1d1472e6b72f43f5fc09e50420a43
Manish Kumar <30774250+manish-jangra@users.noreply.github.com>
2024-08-19 12:51:32 +0530

    KFLUXINFRA-777: Config to Configure Worker Nodes with NVMe Disks

---
+++ configs/nvme-storage-configurator/base/install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Enable debugging
+set -eux
+
+if [ -f /host-etc-systemd-dir/nvme-init-done ]; then
+    echo 'NVMe init already done'
+    oc adm uncordon $NODE_NAME
+    exit 0
+fi
+
+cp /scripts/mount.sh /host-usr-local-bin/mount.sh
+chmod 0755 /host-usr-local-bin/mount.sh
+
+cp /scripts/nvme-storage.service /host-etc-systemd-dir/nvme-storage.service
+chmod 0644 /host-etc-systemd-dir/nvme-storage.service
+
+oc adm cordon $NODE_NAME
+oc adm drain $NODE_NAME --delete-emptydir-data --ignore-daemonsets --grace-period=-1
+
+nsenter -t 1 -m -u -i -n -p -- systemctl daemon-reload
+nsenter -t 1 -m -u -i -n -p -- systemctl enable nvme-storage.service
+touch /host-etc-systemd-dir/nvme-init-done
+nsenter -t 1 -m -u -i -n -p -- systemctl reboot
