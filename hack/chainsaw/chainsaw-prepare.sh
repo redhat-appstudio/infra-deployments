0bfd5d3538a1c29f40686a1492e8892fce3c7bb2
Francesco Ilario <filario@redhat.com>
2025-04-30 15:54:41 +0200

    make chainsaw tests more robust (#6113)
    * make chainsaw tests more robust
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    * use helm in chainsaw overlay
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    ---------
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- hack/chainsaw/chainsaw-prepare.sh
+++ hack/chainsaw/chainsaw-prepare.sh
@@ -6,8 +6,11 @@ set -e
 kind create cluster --name infra-deployments-chainsaw
 
 ## Install kyverno
-kustomize build components/kyverno/chainsaw | \
+kustomize build --enable-helm components/kyverno/chainsaw | \
   kubectl apply -f - --server-side
 
 ## wait for kyverno to rollout
-kubectl rollout status --namespace kyverno deployment --selector '!job-name' --timeout=300s
+kubectl rollout status deployment \
+  --namespace konflux-kyverno \
+  --selector '!job-name' \
+  --timeout=300s
