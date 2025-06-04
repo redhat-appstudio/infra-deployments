10debb0fcd23538886bb01fc69af836baa577f1a
Gal Ben Haim <gbenhaim@redhat.com>
2025-06-04 22:24:32 +0300

    development: Ensure tenant label exists on  Kubesaw created namespaces
    Signed-off-by: Gal Ben Haim <gbenhaim@redhat.com>

--- hack/sandbox-development-mode.sh
+++ hack/sandbox-development-mode.sh
@@ -11,3 +11,12 @@ echo "Installing the Toolchain (Sandbox) operators in dev environment:"
 rm -rf ${TOOLCHAIN_E2E_TEMP_DIR} 2>/dev/null || true
 git clone --depth=1 https://github.com/codeready-toolchain/toolchain-e2e.git ${TOOLCHAIN_E2E_TEMP_DIR}
 make -C ${TOOLCHAIN_E2E_TEMP_DIR} appstudio-dev-deploy-latest SHOW_CLEAN_COMMAND="make -C ${TOOLCHAIN_E2E_TEMP_DIR} appstudio-cleanup" CI_DISABLE_PAIRING=true
+
+# Ensure namespaces created by Kubesaw has the new label
+kubectl get -n toolchain-host-operator -o name tiertemplate | grep tenant | xargs kubectl patch -n toolchain-host-operator --type='json' -p='[
+  {
+    "op": "add",
+    "path": "/spec/template/objects/0/metadata/labels/konflux-ci.dev~1type",
+    "value": "tenant"
+  }
+]'
