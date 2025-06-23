0461628eebd90c8fc013cc0b0c229feafcae9470
Francesco Ilario <filario@redhat.com>
2025-06-23 19:29:27 +0200

    improve formatting for GitHub
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- hack/forbid-clusterpolicies.sh
+++ hack/forbid-clusterpolicies.sh
@@ -2,9 +2,11 @@
 
 set -e -o pipefail
 
+declare OUTPUT
+
 # build manifests and filter ClusterPolicies for provided folder
 folder="${1}"
-manifests=$(kustomize build --enable-helm "${folder}" | yq -o=j)
+manifests=$(kustomize build --enable-helm "${folder}" 2>/dev/null | yq -o=j)
 policies=$(echo "${manifests}"| jq 'select(.kind=="ClusterPolicy")' | jq -s '.')
 
 # calculate the number of policies in the given folder
@@ -14,7 +16,13 @@ num_policies=$(echo "${policies}" | jq 'length')
 if [[ "${num_policies}" -gt 0 ]]; then
   names=$(echo "${policies}" | jq -c 'map(.metadata.name)')
   message="unallowed ClusterPolicies found"
-  jq -n --arg m "${message}" --arg p "${folder}" --argjson n "${names}" '{"message": $m, "path": $p, "policies": $n}'
+
+  if [[ "${OUTPUT}" == "GITHUB" ]]; then
+    fmt_policies="$(echo "${names}" | jq 'join(", ")')"
+    printf "::error title=%s::violated at '%s' by policies '%s'\n" "${message}" "${folder}" "${fmt_policies}"
+  else
+    jq -n --arg m "${message}" --arg p "${folder}" --argjson n "${names}" '{"message": $m, "path": $p, "policies": $n}'
+  fi
   exit 1
 fi
 
