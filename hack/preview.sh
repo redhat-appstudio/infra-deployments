976790f55847c938f33cd12c131ac0ca39940541
Francesco Ilario <filario@redhat.com>
2025-06-25 10:36:34 +0200

    use yq as kustomize is not available at preview.sh execution time
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- hack/preview.sh
+++ hack/preview.sh
@@ -188,9 +188,7 @@ if [[ "$OCP_MINOR" -lt 16 ]]; then
             | .kind=\"ApplicationSet\"
             | .metadata.name = \"kueue\"
             | .\$patch = \"delete\"" >> $ROOT/argo-cd-apps/overlays/development/delete-applications.yaml
-  ( 
-    cd "$ROOT/components/policies/development/" && kustomize edit remove resource 'kueue/'
-  ) || exit 1
+  yq -i 'del(.resources[] | select(test("^kueue/?$")))' "$ROOT/components/policies/development/kustomization.yaml"
 fi
 
 $ROOT/hack/util-set-github-org $MY_GITHUB_ORG
