8e980fba83389a60b0385498b1656c81e0ec9790
Francesco Ilario <filario@redhat.com>
2025-06-27 18:50:10 +0200

    add error handler
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- hack/forbid-clusterpolicies.sh
+++ hack/forbid-clusterpolicies.sh
@@ -2,11 +2,29 @@
 
 set -e -o pipefail
 
+# inputs
 declare OUTPUT
+folder="${1}"
+
+error_handler() {
+  local exit_code=$?
+  local command_line="${BASH_COMMAND}"
+  local line_number="${BASH_LINENO[0]}"
+
+  if [[ "${OUTPUT}" == "GITHUB" ]]; then
+    printf "::error title=Script crashed on line %s::executing '%s' for folder '%s' with exit code %s.\n" "${line_number}" "${command_line}" "${folder}" "${exit_code}"
+  else
+    printf "Script crashed on line %s executing '%s' for folder '%s' with exit code %s.\n" "${line_number}" "${command_line}" "${folder}" "${exit_code}"
+  fi
+
+  exit "${exit_code}"
+}
+
+# Trap the ERR signal and call the error_handler function
+trap error_handler ERR
 
 # build manifests and filter ClusterPolicies for provided folder
-folder="${1}"
-manifests=$(kustomize build --enable-helm "${folder}" 2>/dev/null | yq -o=j)
+manifests=$(kustomize build --enable-helm "${folder}" | yq -o=j)
 policies=$(echo "${manifests}"| jq 'select(.kind=="ClusterPolicy")' | jq -s '.')
 
 # calculate the number of policies in the given folder
@@ -19,7 +37,7 @@ if [[ "${num_policies}" -gt 0 ]]; then
 
   if [[ "${OUTPUT}" == "GITHUB" ]]; then
     fmt_policies="$(echo "${names}" | jq 'join(", ")')"
-    printf "::error title=%s::violated at '%s' by policies '%s'\n" "${message}" "${folder}" "${fmt_policies}"
+    printf "::error title=%s::violated at '%s' by policies '%s'.\n" "${message}" "${folder}" "${fmt_policies}"
   else
     jq -n --arg m "${message}" --arg p "${folder}" --argjson n "${names}" '{"message": $m, "path": $p, "policies": $n}'
   fi
