0007aa5dce19e81a3e0d339ac833c302c1b401df
Emily Keefe <ekeefe@redhat.com>
2025-08-20 10:21:41 -0400

    Add script to manage external Git webhooks
    Assisted by: Cursor

---
+++ hack/git-webhooks/cmd/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	gitlab_webhooks "gitlab-webhooks"
+)
+
+var (
+	create     bool
+	delete     bool
+	webhookURL string
+)
+
+func main() {
+	flag.BoolVar(&create, "create", false, "Flag to create the webhook")
+	flag.BoolVar(&delete, "delete", false, "Flag to delete the webhook")
+	flag.StringVar(&webhookURL, "webhook-url", "", "Required: The webhook URL to reference")
+	flag.Parse()
+
+	// Check if webhook URL is provided
+	if webhookURL == "" {
+		fmt.Fprintf(os.Stderr, "Error: --webhook-url is required\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	// Check that exactly one action is specified
+	if create && delete {
+		fmt.Fprintf(os.Stderr, "Error: cannot specify both --create and --delete\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if create {
+		err := gitlab_webhooks.CreateGitWebhooks(webhookURL)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			os.Exit(1)
+		}
+	} else if delete {
+		err := gitlab_webhooks.DeleteGitWebhooks(webhookURL)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			os.Exit(1)
+		}
+	} else {
+		fmt.Fprintf(os.Stderr, "Error: must specify either --create or --delete\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+}
