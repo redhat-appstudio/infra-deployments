9ba9d0e1f9e9f19d1158c7dc9cfc3b70a1cf3111
Andy Sadler <ansadler@redhat.com>
2025-11-10 14:05:11 -0600

    kueue: add mintmaker resource (#9038)
    Recently, we've been seeing etcd get alarmingly full on stone-prd-rh01.
    Sometimes it's enough for etcd-shield to kick in, which causes a build
    outage that impacts our users (and may impact our SLOs).  We need to
    start looking into methods of reducing the number of resources stored in
    etcd.
    
    One of the causes behind our occupancy issues is mintmaker's update
    checks that run every four hours.  Between the pipelineruns that
    mintmaker generates directly and the on-pull-request pipelineruns
    triggered on downstream repositories, we're generating enough load for
    etcd to fill up.  In particular, we've seen at least 1.5 *MILLION*
    events on the cluster just from these workloads.
    
    To address this issue, we need to reduce the amount of occupancy in
    etcd.  We're currently pursuing multiple methods, but one way to
    prevent mintmaker from overloading our clusters is to get mintmaker's
    pipelineruns to participate in kueue's resource sharing.
    
    Add a new resource type to kueue, "mintmaker", that's set on pipelines
    run in mintmaker's namespace.  With this, we can put an upper bound on
    the number of mintmaker pipelines allowed to run simultaneously; for
    now, set an upper bound of 200.
    
    Part-of: KFLUXINFRA-2526
    
    Signed-off-by: Andy Sadler <ansadler@redhat.com>

--- hack/test-tekton-kueue-config.py
+++ hack/test-tekton-kueue-config.py
@@ -765,6 +765,10 @@ TEST_COMBINATIONS: Dict[str, TestCombination] = {
         "pipelinerun_key": "release_managed",
         "config_key": "production"
     },
+    "mintmaker_production": {
+        "pipelinerun_key": "mintmaker",
+        "config_key": "production"
+    },
 
     # Example: Test the same PipelineRun with different configs to show reusability
     "user-specific-priority_and_mixed_platforms_production-kflux-ocp-p01": {
