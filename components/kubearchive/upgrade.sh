a424704ab52a61c45ecf68267cf5eb95cd13a91c
Hector Martinez Lopez <87312991+rh-hemartin@users.noreply.github.com>
2025-12-01 11:17:57 +0100

    KubeArchive: development upgrade to v1.15.0 (#9368)
    Signed-off-by: Hector Martinez <hemartin@redhat.com>

---
+++ components/kubearchive/upgrade.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+echo "Upgrading KubeArchive from $1 to $2..."
+
+curl -Lo components/kubearchive/development/kubearchive.yaml https://github.com/kubearchive/kubearchive/releases/download/$2/kubearchive.yaml
+cp components/kubearchive/development/kubearchive.yaml components/kubearchive/production/kflux-ocp-p01/kubearchive.yaml
+cp components/kubearchive/development/kubearchive.yaml components/kubearchive/production/kflux-osp-p01/kubearchive.yaml
+cp components/kubearchive/development/kubearchive.yaml components/kubearchive/production/kflux-prd-rh02/kubearchive.yaml
+cp components/kubearchive/development/kubearchive.yaml components/kubearchive/production/kflux-prd-rh03/kubearchive.yaml
+cp components/kubearchive/development/kubearchive.yaml components/kubearchive/production/kflux-rhel-p01/kubearchive.yaml
+cp components/kubearchive/development/kubearchive.yaml components/kubearchive/production/pentest-p01/kubearchive.yaml
+cp components/kubearchive/development/kubearchive.yaml components/kubearchive/production/stone-prd-rh01/kubearchive.yaml
+cp components/kubearchive/development/kubearchive.yaml components/kubearchive/production/stone-prod-p01/kubearchive.yaml
+cp components/kubearchive/development/kubearchive.yaml components/kubearchive/production/stone-prod-p02/kubearchive.yaml
+sed -i "s/$1/$2/g" components/kubearchive/production/**/kustomization.yaml components/kubearchive/development/kustomization.yaml
