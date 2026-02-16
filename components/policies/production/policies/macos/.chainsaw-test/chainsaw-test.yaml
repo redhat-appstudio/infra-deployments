redhat-appstudio/infra-deployments

commit face5c64faf7b310edd2cf9d2efa64d629ab29c0

reverts #10502 (#10543)
Signed-off-by: Francesco Ilario <filario@redhat.com>

rh-pre-commit.version: 2.3.2
rh-pre-commit.check-secrets: ENABLED

components/policies/production/stone-prod-p02/kustomization.yaml
@@ -7,5 +7,5 @@ resources:
 - ../policies/kubearchive/
 - ../policies/kueue/
 - ../policies/build-resources/
-- ../policies/macos/
-- ./macos-config.yaml
+# - ../policies/macos/
+# - ./macos-config.yaml

commit 08be8d94dbcf33e3de1f3f8d24a95ac004e1852a

policies: restrict macos namespaces (#10546)
Mac OS builds are currently extremely expensive for us to perform, so
let's restrict the tenants that are allowed to make Mac OS builds to an
allowlist.

v2: use kyverno policies, since validation admission policies are not in
GA yet and have some bugs.

v3: more tests, since we caused an outage and had to be reverted in
face5c64f ("reverts #10502 (#10543)")

Fixes: face5c64f ("reverts #10502 (#10543)")
Fixes: KFLUXINFRA-2811

Signed-off-by: Andy Sadler <ansadler@redhat.com>

components/policies/production/policies/macos/restrict-macos-runners.yaml
@@ -17,12 +17,16 @@ spec:
       matchLabels:
         konflux-ci.dev/type: tenant
   variables:
+    - name: annotationName
+      expression: '"kueue.konflux-ci.dev/requests-macos-mac2metal-arm64"'
     - name: isRestrictedNamespace
       expression: |
         resource.Get("v1", "configmaps", "konflux-policies", "macos-allowlist").data.namespaces.split('\n').all(n, n != object.metadata.namespace)
     - name: isRestrictedPlatform
       expression: |
-        uint(object.metadata.annotations["kueue.konflux-ci.dev/requests-macos-mac2metal-arm64"]) > 0
+        has(object.metadata.annotations) &&
+          object.metadata.annotations.exists(k, k == variables.annotationName) ?
+          int(object.metadata.annotations[variables.annotationName]) > 0 : false
   validations:
     - messageExpression: "'namespace ' + object.metadata.namespace + ' is not allowed to acquire macos runners'"
       expression: |

components/policies/production/stone-prod-p02/kustomization.yaml
@@ -7,5 +7,5 @@ resources:
 - ../policies/kubearchive/
 - ../policies/kueue/
 - ../policies/build-resources/
-# - ../policies/macos/
-# - ./macos-config.yaml
+- ../policies/macos/
+- ./macos-config.yaml

components/policies/production/policies/macos/.chainsaw-test/chainsaw-test.yaml
@@ -126,7 +126,7 @@ metadata:
   name: multiple-namespaces
 spec:
   description: |
-    Multiple namespaces can be alloed, and they should all be allowed to acquire macos runners
+    Multiple namespaces can be in the allowlist, and they should all be allowed to acquire macos runners
   concurrent: false
   namespaceTemplate:
     metadata:
@@ -202,6 +202,130 @@ spec:
         bindings:
         - name: destNamespace
           value: ($secondNamespace)
+        expect:
+        - check:
+            ($error == null): true
+---
+apiVersion: chainsaw.kyverno.io/v1alpha1
+kind: Test
+metadata:
+  name: allow-non-macos-pipelineruns-in-allowed-namespaces
+spec:
+  description: |
+    Ensure that pipelineruns not requesting macos VMs are admitted to the cluster.
+  concurrent: false
+  namespaceTemplate:
+    metadata:
+      labels:
+        konflux-ci.dev/type: tenant
+  bindings:
+  - name: suffix
+    value: konfluxcidev
+  steps:
+  - name: given-pipelineruns-exist
+    try:
+    - apply:
+        file: ./resources/pipelineruns.tekton.dev.yaml
+    cleanup:
+    - delete:
+        file: ./resources/pipelineruns.tekton.dev.yaml
+  - name: given-configmap-exists
+    bindings:
+    - name: allowedNamespace
+      value: ($namespace)
+    try:
+    - apply:
+        resource:
+          apiVersion: v1
+          kind: Namespace
+          metadata:
+            name: konflux-policies
+    - apply:
+        file: ./resources/macos-config.yaml
+        template: true
+    - assert:
+        resource:
+          apiVersion: v1
+          kind: ConfigMap
+          metadata:
+            name: macos-allowlist
+            namespace: konflux-policies
+          data:
+            namespaces: ($allowedNamespace)
+  - name: given-admission-policy-exists
+    try:
+    - apply:
+        file: ../restrict-macos-runners.yaml
+  - name: then-pipelinerun-is-created
+    try:
+    - create:
+        resource:
+          apiVersion: tekton.dev/v1
+          kind: PipelineRun
+          metadata:
+            name: foo
+        expect:
+        - check:
+            ($error == null): true
+---
+apiVersion: chainsaw.kyverno.io/v1alpha1
+kind: Test
+metadata:
+  name: allow-non-macos-pipelineruns-in-disallowed-namespaces
+spec:
+  description: |
+    Ensure that pipelineruns not requesting macos VMs in disallowed namespaces are admitted to the cluster.
+  concurrent: false
+  namespaceTemplate:
+    metadata:
+      labels:
+        konflux-ci.dev/type: tenant
+  bindings:
+  - name: suffix
+    value: konfluxcidev
+  steps:
+  - name: given-pipelineruns-exist
+    try:
+    - apply:
+        file: ./resources/pipelineruns.tekton.dev.yaml
+    cleanup:
+    - delete:
+        file: ./resources/pipelineruns.tekton.dev.yaml
+  - name: given-configmap-exists
+    bindings:
+    - name: allowedNamespace
+      value: (join('-', [$namespace, 'foo']))
+    try:
+    - apply:
+        resource:
+          apiVersion: v1
+          kind: Namespace
+          metadata:
+            name: konflux-policies
+    - apply:
+        file: ./resources/macos-config.yaml
+        template: true
+    - assert:
+        resource:
+          apiVersion: v1
+          kind: ConfigMap
+          metadata:
+            name: macos-allowlist
+            namespace: konflux-policies
+          data:
+            namespaces: ($allowedNamespace)
+  - name: given-admission-policy-exists
+    try:
+    - apply:
+        file: ../restrict-macos-runners.yaml
+  - name: then-pipelinerun-is-created
+    try:
+    - create:
+        resource:
+          apiVersion: tekton.dev/v1
+          kind: PipelineRun
+          metadata:
+            name: foo
         expect:
         - check:
             ($error == null): true