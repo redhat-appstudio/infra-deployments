97d13f078a288f9d37dc44a187a32b3d37a051b4
Francesco Ilario <filario@redhat.com>
2026-08-06 19:57:06 +0200

    update script to generate allowed annotations
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    rh-pre-commit.version: 2.4.0
    rh-pre-commit.check-secrets: ENABLED

--- hack/kueue-allowed-vms/generate-allowed-annotations.sh
+++ hack/kueue-allowed-vms/generate-allowed-annotations.sh
@@ -4,31 +4,67 @@ set -euo pipefail
 
 declare -r ROOT="${BASH_SOURCE[0]%/*}"
 
+allowed_resources() {
+  kustomize build "${1}" | \
+    yq 'select(.kind == "ClusterQueue")' | \
+    yq '[.spec.resourceGroups[].coveredResources[]]' | \
+    yq '. |= del(.[] | select(. == "tekton.dev/pipelineruns"))' | \
+    yq '[.[] | "kueue.konflux-ci.dev/requests-" + . ] | sort' -o=json -I=0
+}
+
 update_configmap_from_clusterqueue() {
-  ALLOWED_RESOURCES=$(kustomize build "${1}" | \
-    yq 'select(.kind=="ClusterQueue")' | \
-    yq '[.spec.resourceGroups[].coveredResources[] | "kueue.konflux-ci.dev/requests-" + . ] ' -o json -I=0) \
-      yq -i '.data.allowed-annotations = env(ALLOWED_RESOURCES)' "${2}"
+  ALLOWED_RESOURCES=$(allowed_resources "${1}") \
+    yq -i '.data.allowed-annotations = (env(ALLOWED_RESOURCES)| to_string)' "${2}"
+}
+
+update_test_resources_from_clusterqueue() {
+  # calculates the allowed resources
+  ALLOWED_RESOURCES=$(allowed_resources "${1}")
+
+  # updates the test PipelineRun with cluster specific allowed annotations
+  TEST_ALLOWED_PLR_FILEPATH='.chainsaw-test/resources/pipelinerun_with_allowed_annotations.yaml'
+  local allowed_plr_filepath="${2%/}/${TEST_ALLOWED_PLR_FILEPATH}"
+  echo "Generating Test File: $input_file -> $allowed_plr_filepath"
+  ALLOWED_RESOURCES=${ALLOWED_RESOURCES} \
+    yq -P -i '.metadata.annotations = (
+      strenv(ALLOWED_RESOURCES) | from_json |
+      map({"key": ., "value": "1"}) | from_entries)' "${allowed_plr_filepath}"
+
+  # updates the test PipelineRun with cluster specific allowed and unallowed annotations
+  TEST_UNALLOWED_PLR_FILEPATH='.chainsaw-test/resources/pipelinerun_with_allowed_and_unallowed_annotations.yaml'
+  local unallowed_plr_filepath="${2%/}/${TEST_UNALLOWED_PLR_FILEPATH}"
+  echo "Generating Test File: $input_file -> $unallowed_plr_filepath"
+  ALLOWED_RESOURCES=${ALLOWED_RESOURCES} \
+    yq -P -i '.metadata.annotations = (
+      strenv(ALLOWED_RESOURCES) | from_json | . += "kueue.konflux-ci.dev/requests-unallowed-resource" |
+      map({"key": ., "value": "1"}) |
+      from_entries)' "${unallowed_plr_filepath}"
 }
 
 main() {
-    # Define input-output file pairs
-    local -A queue_configs=(
-        ["components/kueue/staging/stone-stage-p01"]="components/policies/staging/stone-stage-p01/kueue/deny-unallowed-annotations/deny-unallowed-annotations-configmap.yaml"
-        ["components/kueue/staging/stone-stg-rh01"]="components/policies/staging/stone-stg-rh01/kueue/deny-unallowed-annotations/deny-unallowed-annotations-configmap.yaml"
-    )
+  CONFIGMAP_FILENAME='deny-unallowed-annotations-configmap.yaml'
 
-    for input_file in "${!queue_configs[@]}"; do
-        local output_file="${queue_configs[$input_file]}"
-        echo "Generating queue config: $input_file -> $output_file"
+  # Define input-output file pairs
+  local -A queue_configs=(
+      ["components/kueue/staging/stone-stage-p01"]="components/policies/staging/stone-stage-p01/kueue/deny-unallowed-annotations/"
+      ["components/kueue/staging/stone-stg-rh01"]="components/policies/staging/stone-stg-rh01/kueue/deny-unallowed-annotations/"
+  )
 
-        update_configmap_from_clusterqueue "${input_file}" "${output_file}"
-    done
+  for input_file in "${!queue_configs[@]}"; do
+    local output_folder="${queue_configs[$input_file]%/}"
+    # Update ConfigMap
+    local output_cm_file="${output_folder}/${CONFIGMAP_FILENAME}"
+    echo "Generating queue config: $input_file -> $output_cm_file"
+    update_configmap_from_clusterqueue "${input_file}" "${output_cm_file}"
+
+    # Update Test Resources
+    update_test_resources_from_clusterqueue "${input_file}" "${output_folder}"
+  done
 }
 
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
-    (
-        cd "$ROOT/../.."
-        main
-    )
+  (
+      cd "$ROOT/../.."
+      main
+  )
 fi
